560f4c39a84d7a976e4cd46c0c256d211399f44e
Efeoseaje <efeoseaje@yahoo.com>
2023-07-25 17:11:51 +0100

    Print number combination

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Print Number comninations """
+
+for i in range(0, 10):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print(89)
+        else:
+            print('{}{}'.format(i, j), end=", ")
