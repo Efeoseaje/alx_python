bf40a58e3466c9ab72453ee184a816d8c737428b
Efeoseaje <efeoseaje@yahoo.com>
2023-08-02 00:59:45 +0100

    Access and update private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+""" Defines a Square"""
+
+
+class Square:
+    """ a class that defines a Square """
+    
+    def __init__(self, size=0):
+        """ Initializes the square class
+        Args: 
+            size: represents the size of the square
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ retrieves the size """
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """ Sets the size """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Computes and returns the current square area """
+        return (self.__size ** 2)
