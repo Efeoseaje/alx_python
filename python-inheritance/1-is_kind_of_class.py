174f030e8005503c119dcafb28c64a8136dff295
Efeoseaje <efeoseaje@yahoo.com>
2023-08-02 23:41:51 +0100

    Same class or inherit from

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,9 +1,9 @@
-"""Checks if object is an exact instance of a class"""
+"""Checks if object is an instance of a class"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ Returns True if the object is exactly an instance of the class """
-    if type(obj) == a_class:
+    if isinstance(obj, a_class):
         return True
     else:
         return False
