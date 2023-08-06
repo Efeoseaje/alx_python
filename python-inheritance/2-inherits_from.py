623cb782808491430a6ea48772cd43de7bcb4d52
Efeoseaje <efeoseaje@yahoo.com>
2023-08-06 23:30:27 +0100

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,9 +1,9 @@
 """Checks if object is a subclass of a class"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ Returns True if the object is a subclass of the class """
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
