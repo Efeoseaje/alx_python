d8a42597ebf17c260aba225edcc4193f42a5927b
Efeoseaje <efeoseaje@yahoo.com>
2023-08-07 00:22:09 +0100

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,32 @@
 """An empty class"""
 
 
-class BaseGeometry:
+from collections.abc import Iterable
+
+
+class MetaGeometry(type):
+    """ A class used to define the behaviour of class BaseGeometry"""
+
+    def __dir__(MetaGeometry):
+        """ A special method used to customize the behaviour of dir()
+        Args:
+            MetaGeometry: The metaclass to customize it's attributes
+        
+        Return:
+            Returns all attributes of the metaclass except '__init_subclass__' attribute
+        """
+        return [attributes for attributes in super().__dir__() if attributes != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaGeometry):
     """This is an empty class"""
-    pass
+    
+    def __dir__(BaseGeometry):
+        """ A special method used to customize the behaviour of dir()
+        Args:
+            BaseGeometry: The class to customize it's attributes
+        
+        Return:
+            Returns all attributes of the class except '__init_subclass__' attribute
+        """
+        return [attributes for attributes in super().__dir__() if attributes != '__init_subclass__']
