54ba61d304847ecac9c865f152c2ecd788add29a
Efeoseaje <efeoseaje@yahoo.com>
2023-08-07 01:44:14 +0100

    Square #1

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,28 @@
+""" A Square class"""
+
+
+Rectangle = __import__('7-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ A class Square that inherits from Rectangle class """
+
+    def __init__(self, size):
+        """ Instantiation of the Square class
+        Args:
+            size(int): size of the Square.
+            
+        raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less or equal to 0.
+        """
+        self.__size = size # stores the sides in a private attribute 
+        self.integer_validator("size", size)
+        super().__init__(size, size) # instantiation of the square with two attributes(width and height which are the same)
+
+    def area(self):
+        """ Computes the area of the Square 
+        Returns:
+            The area of the Square
+        """
+        return super().area() # utilizes the area method from the rectangle
