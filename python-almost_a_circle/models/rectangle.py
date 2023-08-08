21b9563be2c4cee0d384481d72bd6df872fb0a2d
Efeoseaje <efeoseaje@yahoo.com>
2023-08-08 19:28:39 +0100

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,60 @@
+""" A Rectangle class that inherits from Base class """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """  A class that defines a rectangle and inherits from base class """
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Instantiation of the Rectangle class
+        Args:
+            width(int): the width of the rectangle
+            height(int): the height of the rectangle
+            x(int): the x cordinate of the rectangle
+            y(int): the y cordinate of the rectangle
+            id(int): the identity of the rectangle.
+        Raises:
+            TypeError: if width, height, x, y is not an int.
+            ValueError: if width, height is <= 0.
+            ValueError: if x, y is < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+
+    @property
+    def width(self):
+        """ Retrieve the width """
+        return self.__width
+    
+    @property
+    def height(self):
+        """ Retrieve the height """
+        return self.__height
+    
+    @property
+    def x(self):
+        """ Retrieve the x-cordinate """
+        return self.__x
+    
+    @property
+    def y(self):
+        """ Retrieve the y-cordinate """
+        return self.__y
+    
+
+    @width.setter
+    def width(self, value):
+        """ Setting the right value for width """
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+
+
