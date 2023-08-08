68f0b4dc403754103e0edc0ceefe005ff0cea431
Efeoseaje <efeoseaje@yahoo.com>
2023-08-08 19:31:06 +0100

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -56,5 +56,13 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
 
+    @height.setter
+    def height(self, value):
+        """ Setting the right value for height """
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
 
 
