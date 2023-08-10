7773a5282644bd6c53e49d1e25c61510bdc24d94
Efeoseaje <efeoseaje@yahoo.com>
2023-08-10 02:21:23 +0100

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -9,4 +9,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+    # print(response.headers['X-Request-Id'])
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print("{}".format(x_request_id))
+    else:
+        print("X-Request-Id header not found in the response.")
