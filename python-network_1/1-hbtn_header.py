2770710f387d5a7eae89f4aed230359bef3a8793
Efeoseaje <efeoseaje@yahoo.com>
2023-08-10 02:27:15 +0100

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,15 +3,36 @@ displays the value of the variable X-Request-Id
 """
 
 
+# import requests
+# import sys
+# 
+# if __name__ == "__main__":
+    # url = sys.argv[1]
+    # response = requests.get(url)
+    # # print(response.headers['X-Request-Id'])
+    # x_request_id = response.headers.get('X-Request-Id')
+    # if x_request_id:
+        # print("{}".format(x_request_id))
+    # else:
+        # print("X-Request-Id header not found in the response.")
 import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
+
     url = sys.argv[1]
-    response = requests.get(url)
-    # print(response.headers['X-Request-Id'])
-    x_request_id = response.headers.get('X-Request-Id')
-    if x_request_id:
-        print("{}".format(x_request_id))
-    else:
-        print("X-Request-Id header not found in the response.")
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx or 5xx)
+        
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print("{}".format(x_request_id))
+        else:
+            print("X-Request-Id header not found in the response.")
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while making the request:", e)
