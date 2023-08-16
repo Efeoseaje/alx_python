e3490f1de9d5ea53277b0b80854432d2924216c5
Efeoseaje <efeoseaje@yahoo.com>
2023-08-16 11:47:57 +0100

    Error code #1

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -13,3 +13,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
+    else:
+        print('Regular request')
