ddb2cf9f0e83f3163173db799a7d0bd6dfa4122f
Efeoseaje <efeoseaje@yahoo.com>
2023-08-16 16:25:47 +0100

    My GitHub!

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,21 @@
+"""Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit(1)
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        r = response.json()
+        print("{}".format(r.get("id")))
