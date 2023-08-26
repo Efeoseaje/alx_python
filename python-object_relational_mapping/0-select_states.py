fc7fe34055a258eaebdca9bcddb6ceb0c4f7071d
Efeoseaje <efeoseaje@yahoo.com>
2023-08-26 22:21:35 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 """ script that lists all states from the database hbtn_0e_0_usa """
 
-import MySQLdb as DB
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
     # Create a database connection
-    db_connect = DB.connect(
+    db_connect = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=sys.argv[0],
