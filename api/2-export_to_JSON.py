cd0c589b33832726b421db87ee1645a8010cd212
Efeoseaje <efeoseaje@yahoo.com>
2023-10-09 13:19:15 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress and
+exports it to JSON format
+"""
+
 import json
 import requests
 import sys
