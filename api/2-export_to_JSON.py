c175db945a791e178e05fe020a4fb7c7971a1699
Efeoseaje <efeoseaje@yahoo.com>
2023-10-09 13:16:23 +0100

    Export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,58 @@
+import json
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    # Define the base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Fetch employee details
+    employee_url = f"{base_url}/users/{employee_id}"
+    employee_response = requests.get(employee_url)
+
+    if employee_response.status_code != 200:
+        print(f"Failed to retrieve employee details for ID {employee_id}.")
+        return
+
+    employee_data = employee_response.json()
+    employee_username = employee_data["username"]
+
+    # Fetch employee's TODO list
+    todo_url = f"{base_url}/users/{employee_id}/todos"
+    todo_response = requests.get(todo_url)
+
+    if todo_response.status_code != 200:
+        print(f"Failed to retrieve TODO list for employee {employee_username}.")
+        return
+
+    todo_data = todo_response.json()
+
+    # Export data to JSON format
+    json_filename = f"{employee_id}.json"
+    json_data = {"USER_ID": []}
+    for task in todo_data:
+        task_completed_status = True if task["completed"] else False
+        json_data["USER_ID"].append({
+            "task": task["title"],
+            "completed": task_completed_status,
+            "username": employee_username
+        })
+
+    with open(json_filename, mode='w') as json_file:
+        json.dump(json_data, json_file, indent=4)
+
+    print(f"JSON file '{json_filename}' has been created with TODO list data for employee {employee_username}.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
